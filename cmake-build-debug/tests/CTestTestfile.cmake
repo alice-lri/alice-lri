b6214f48c39bb2361b9aaf73af50634bf3ad88c7
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-02-10 13:24:09 +0100

    Minor env updates

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samuel.soutullo/GitHub/AccurateRI/tests
-# Build directory: /home/samuel.soutullo/GitHub/AccurateRI/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[accurate_ri_tests]=] "/home/samuel.soutullo/GitHub/AccurateRI/cmake-build-debug/tests/run_tests")
-set_tests_properties([=[accurate_ri_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/samuel.soutullo/GitHub/AccurateRI/tests/CMakeLists.txt;4;add_test;/home/samuel.soutullo/GitHub/AccurateRI/tests/CMakeLists.txt;0;")
