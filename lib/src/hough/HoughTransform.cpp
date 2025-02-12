32d5e46e0bedaf11e367f73a1d8d8d3554d16289
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-02-12 19:11:43 +0100

    More minor refactors

--- lib/src/hough/HoughTransform.cpp
+++ lib/src/hough/HoughTransform.cpp
@@ -36,47 +36,6 @@ namespace accurate_ri {
         LOG_INFO("Accumulator computation completed.");
     }
 
-    std::optional<std::pair<uint64_t, uint64_t>> HoughTransform::findMaximum(std::optional<double> averageX) {
-        std::vector<std::pair<size_t, size_t>> maxIndices;
-        double maxVal = -std::numeric_limits<double>::infinity();
-
-        for (size_t y = 0; y < yCount; y++) {
-            for (size_t x = 0; x < xCount; x++) {
-                const double val = accumulator[y * xCount + x];
-                if (val > maxVal) {
-                    maxVal = val;
-                    maxIndices = {{x, y}};
-                } else if (val == maxVal) {
-                    maxIndices.emplace_back(x, y);
-                }
-            }
-        }
-
-        if (maxVal <= 1e-6 || maxIndices.empty()) {
-            LOG_INFO("No maxima found in the accumulator.");
-            return std::nullopt;
-        }
-
-        if (maxIndices.size() == 1 || !averageX) {
-            return maxIndices[maxIndices.size() / 2];
-        }
-
-        auto closestPair = maxIndices[0];
-        double minDistance = std::numeric_limits<double>::infinity();
-
-        for (const auto &[x, y] : maxIndices) {
-            const double xValue = xMin + xStep * static_cast<double>(x);
-            const double distance = std::abs(xValue - *averageX);
-
-            if (distance < minDistance) {
-                minDistance = distance;
-                closestPair = {x, y};
-            }
-        }
-
-        return closestPair;
-    }
-
     inline void HoughTransform::updateAccumulatorForPoint(
         const uint64_t pointIndex, const PointArray &points, const std::vector<double> &xValues
     ) {
@@ -121,5 +80,46 @@ namespace accurate_ri {
             hashAccumulator[yBetween * xCount + x - 1] ^= HashUtils::knuth_uint(pointIndex);
             hashAccumulator[yBetween * xCount + x] ^= HashUtils::knuth_uint(pointIndex);
         }
+    }
+
+    std::optional<std::pair<uint64_t, uint64_t>> HoughTransform::findMaximum(std::optional<double> averageX) {
+        std::vector<std::pair<size_t, size_t>> maxIndices;
+        double maxVal = -std::numeric_limits<double>::infinity();
+
+        for (size_t y = 0; y < yCount; y++) {
+            for (size_t x = 0; x < xCount; x++) {
+                const double val = accumulator[y * xCount + x];
+                if (val > maxVal) {
+                    maxVal = val;
+                    maxIndices = {{x, y}};
+                } else if (val == maxVal) {
+                    maxIndices.emplace_back(x, y);
+                }
+            }
+        }
+
+        if (maxVal <= 1e-6 || maxIndices.empty()) {
+            LOG_INFO("No maxima found in the accumulator.");
+            return std::nullopt;
+        }
+
+        if (maxIndices.size() == 1 || !averageX) {
+            return maxIndices[maxIndices.size() / 2];
+        }
+
+        auto closestPair = maxIndices[0];
+        double minDistance = std::numeric_limits<double>::infinity();
+
+        for (const auto &[x, y] : maxIndices) {
+            const double xValue = xMin + xStep * static_cast<double>(x);
+            const double distance = std::abs(xValue - *averageX);
+
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestPair = {x, y};
+            }
+        }
+
+        return closestPair;
     }
 } // namespace accurate_ri
