fe0cb3fa05b366e1f292f6f18371af4ebd802a45
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-02-24 21:02:13 +0100

    Parsing argc argv

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <optional>
 #include "accurate_ri.h"
 #include "FileUtils.h"
@@ -12,11 +13,31 @@ void setCloudPath(const std::string& path) {
     accurate_ri::setCloudPath(newPath);
 }
 
-int main() {
-    const std::string path = "../../Datasets/LiDAR/durlar/dataset/DurLAR/DurLAR_20211209/ouster_points/data/0000009025.bin";
-    const std::optional<int> accurateDigits = 4;
-    FileUtils::Points points = FileUtils::loadBinaryFile(path, accurateDigits);
+int main(int argc, char** argv) {
+    std::string path;
+    std::optional<int> accurateDigits = std::nullopt;
 
+    switch (argc) {
+        case 1:
+            std::cout << "Using default parameters" << std::endl;
+            path = "../../Datasets/LiDAR/durlar/dataset/DurLAR/DurLAR_20211209/ouster_points/data/0000009025.bin";
+            accurateDigits = std::nullopt;
+            break;
+        case 2:
+            path = argv[1];
+            break;
+        case 3:
+            path = argv[1];
+            accurateDigits = std::stoi(argv[2]);
+            break;
+        default:
+            std::cout << "Usage: " << argv[0] << " [path] [accurateDigits]" << std::endl;
+            return 1;
+    }
+
+    FileUtils::Points points = FileUtils::loadBinaryFile(path, accurateDigits);
     setCloudPath(path);
+
     accurate_ri::execute(points.x, points.y, points.z);
+    return 0;
 }
