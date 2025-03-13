2926a922c2e0df890c7cf1848227995e50e66852
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-03-13 16:17:53 +0100

    Added conda initialization inside task.sh

--- scripts/cesga/task.sh
+++ scripts/cesga/task.sh
@@ -6,5 +6,7 @@ EXEC_FILE=$(basename "$EXECUTABLE_PATH")
 cd "$EXEC_DIR" || { echo "Failed to cd into $EXEC_DIR"; exit 1; }
 cp "${DB_DIR}/initial.sqlite" "${DB_DIR}/${SLURM_PROCID}.sqlite"
 
+eval "$(conda shell.bash hook)"
 conda activate "${CONDA_ENV_NAME}"
+
 ./"$EXEC_FILE" "$SLURM_PROCID" "$SLURM_NTASKS"
