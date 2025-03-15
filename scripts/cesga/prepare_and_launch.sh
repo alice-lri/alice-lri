5455e7cecab97457850a20e72df81c08ca270336
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-03-15 13:06:59 +0100

    Changed CESGA db path

--- scripts/cesga/prepare_and_launch.sh
+++ scripts/cesga/prepare_and_launch.sh
@@ -3,7 +3,7 @@ set -e
 
 ## Adjust these as needed
 CONDA_ENV_NAME="accurate_ri_env"
-BASE_DB_DIR="${STORE2}/accurate_ri"
+BASE_DB_DIR="${STORE2}/accurate_ri_db"
 KITTI_PATH="${STORE2}/datasets_lidar/kitti"
 DURLAR_PATH="${STORE2}/datasets_lidar/durlar/dataset/DurLAR"
 SRC_PATH="../.."
