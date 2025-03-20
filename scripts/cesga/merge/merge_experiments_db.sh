4660a30e67443d57aaa31a99e96e5b1822b0f8f6
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-03-20 16:45:09 +0100

    CESGA: Fixed conda path on merge scripts

--- scripts/cesga/merge/merge_experiments_db.sh
+++ scripts/cesga/merge/merge_experiments_db.sh
@@ -6,7 +6,7 @@ source helper/merge_header.sh
 
 echo "Merging experiments databases from ${TARGET_DIR}..."
 
-source init_conda.sh
+source ../conda/init_conda.sh
 python helper/merge_db.py "$TARGET_DIR" "$BASE_DB_DIR/master.sqlite" --type="experiments"
 
 echo "Experiments database merged successfully."
