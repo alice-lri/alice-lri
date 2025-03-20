7e4e8356d6c62918daa7e0bc96a607118acf9dab
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-03-20 13:15:40 +0100

    CESGA: Multi batch job script refactoring

--- scripts/cesga/estimate/pre_job.py
+++ scripts/cesga/estimate/pre_job.py
@@ -1,6 +1,11 @@
+import argparse
 import sqlite3
 
-conn = sqlite3.connect("../../examples/experiments/experiments.sqlite")
+parser = argparse.ArgumentParser(description='Prepare the DB for a new experiment.')
+parser.add_argument('db_path', type=str, help='Path to the SQLite database')
+args = parser.parse_args()
+
+conn = sqlite3.connect(args.db_path)
 cur = conn.cursor()
 
 cur.execute("INSERT INTO experiment(timestamp) VALUES (datetime('now', 'localtime', 'subsec'))")
--- scripts/cesga/estimate/prepare_and_launch.sh
+++ scripts/cesga/estimate/prepare_and_launch.sh
@@ -3,55 +3,7 @@ set -eo pipefail
 cd "$(dirname "$0")" || exit
 
 source ../helper/paths.sh
-SRC_PATH="../../.."
-EXECUTABLE_NAME="examples_sql"
-JOB_COUNT=32
-
-if [ -n "$1" ]; then
-  BATCH_ID=$1
-  RESUME_BATCH=true
-else
-  BATCH_ID="$(date +'%Y%m%d_%H%M%S_%3N')"
-  RESUME_BATCH=false
-fi
-
-ACTUAL_LOGS_DIR="${BASE_LOGS_DIR}/${BATCH_ID}"
-ACTUAL_DB_DIR="${BASE_DB_DIR}/${BATCH_ID}"
-
-if [ "$RESUME_BATCH" = true ]; then
-  if [ ! -d "${ACTUAL_LOGS_DIR}" ]; then
-    echo "Log directory ${ACTUAL_LOGS_DIR} does not exist."
-    exit 1
-  fi
-
-  if [ ! -d "${ACTUAL_DB_DIR}" ]; then
-    echo "DB directory ${ACTUAL_DB_DIR} does not exist."
-    exit 1
-  fi
-
-  JOBS_TO_RUN=()
-  for i in $(seq 0 $((JOB_COUNT - 1))); do
-    if [ ! -f "${ACTUAL_DB_DIR}/job_${i}.success" ]; then
-      JOBS_TO_RUN+=("$i")
-    fi
-  done
-else
-    for i in $(seq 0 $((JOB_COUNT - 1))); do
-      JOBS_TO_RUN+=("$i")
-    done
-fi
-
-echo "Will run jobs: " "${JOBS_TO_RUN[@]}"
-echo "Continue? (y/n)"
-read -r CONTINUE
-
-if [ "$CONTINUE" != "y" ]; then
-  echo "Aborting."
-  exit 1
-fi
-
-mkdir -p "${ACTUAL_DB_DIR}"
-mkdir -p "${ACTUAL_LOGS_DIR}"
+source ../helper/multi_batch_job_header.sh
 
 echo "Building project..."
 cmake -DCMAKE_BUILD_TYPE=Release -DLOG_LEVEL=INFO -DENABLE_PROFILING=ON -S "${SRC_PATH}" -B "${SRC_PATH}/build"
@@ -60,9 +12,8 @@ make -C "${SRC_PATH}/build"
 source ../conda/init_conda.sh
 
 echo "Preparing job..."
-python pre_job.py
-
-cp ../../examples/experiments/experiments.sqlite "${ACTUAL_DB_DIR}/initial.sqlite"
+cp "${BASE_DB_DIR}/master.sqlite" "${ACTUAL_DB_DIR}/initial.sqlite"
+python pre_job.py "${ACTUAL_DB_DIR}/initial.sqlite"
 
 jq -n \
   --arg db_dir "$ACTUAL_DB_DIR" \
