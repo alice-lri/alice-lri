fc692755f0ac10a184c0d087474d85b65dcc6aa1
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-04-01 16:31:12 +0200

    CESGA: Using initial DB as initial instead of master

--- scripts/cesga/estimate/prepare_and_launch.sh
+++ scripts/cesga/estimate/prepare_and_launch.sh
@@ -14,7 +14,7 @@ make -C "${SRC_PATH}/build"
 source ../conda/init_conda.sh
 
 echo "Preparing job..."
-cp "${BASE_DB_DIR}/master.sqlite" "${ACTUAL_DB_DIR}/initial.sqlite"
+cp "${BASE_DB_DIR}/initial.sqlite" "${ACTUAL_DB_DIR}/initial.sqlite"
 python pre_job.py "${ACTUAL_DB_DIR}/initial.sqlite"
 
 jq -n \
@@ -32,7 +32,7 @@ jq -n \
 
 for i in "${JOBS_TO_RUN[@]}"; do
   echo "Launching job ${i}..."
-  sbatch --job-name="accurate_ri_${i}" -o "${ACTUAL_LOGS_DIR}/${i}.log" -e "${ACTUAL_LOGS_DIR}/${i}.log"\
+  sbatch --job-name="accurate_ri_${i}" -o "${ACTUAL_LOGS_DIR}/${i}.log" -e "${ACTUAL_LOGS_DIR}/${i}.log" \
    job.sh "${CONDA_ENV_NAME}" "${SRC_PATH}/build/examples/${EXECUTABLE_NAME}" "${ACTUAL_DB_DIR}" \
     "${i}" "${JOB_COUNT}"
 done
