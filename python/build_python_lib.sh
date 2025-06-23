fc3b4e842131e990c6a9379c8c8e575a5b1a497a
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-06-23 09:47:02 +0200

    Updated build python lib

--- python/build_python_lib.sh
+++ python/build_python_lib.sh
@@ -2,7 +2,7 @@
 
 rm -Rf build
 conan install ../lib -s compiler.cppstd=gnu20 -s build_type=Release -of build/lib --build=missing
-cmake -DLOG_LEVEL=NONE -DCMAKE_BUILD_TYPE=Release -DENABLE_TRACE_FILE=OFF -B build -G Ninja
+cmake -DCMAKE_BUILD_TYPE=Release -DLOG_LEVEL=NONE -DENABLE_TRACE_FILE=OFF -DENABLE_PROFILING=OFF -DLIB_MODE=ON -B build -G Ninja
 cd build || exit
 ninja
 
