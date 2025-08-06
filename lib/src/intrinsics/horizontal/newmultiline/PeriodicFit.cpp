84ece2f3ab89f0d157de6bd28ac134f93fa970ec
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-08-06 14:39:10 +0200

    Revert reusing buffers: minimal performance gain but less maintainable

--- lib/src/intrinsics/horizontal/newmultiline/PeriodicFit.cpp
+++ lib/src/intrinsics/horizontal/newmultiline/PeriodicFit.cpp
@@ -9,18 +9,16 @@ namespace accurate_ri {
         Eigen::ArrayXi linesIdx;
     };
 
-    NewMultiLineResult buffer;
-
-    double computeCircularMeanIntercept(Eigen::ArrayXd& residuals, const double thetaStep) {
+    double computeCircularMeanIntercept(const Eigen::ArrayXd& residuals, const double thetaStep) {
         constexpr double twoPi = 2.0 * M_PI;
 
-        residuals = residuals.unaryExpr([thetaStep](const double residual) {
+        Eigen::ArrayXd residualsMod = residuals.unaryExpr([thetaStep](const double residual) {
             const double residualMod = std::fmod(residual, thetaStep);
             return residualMod < 0 ? residualMod + thetaStep : residualMod;
         });
 
-        residuals = (twoPi / thetaStep) * residuals;
-        double circularMean = std::atan2(residuals.sin().mean(), residuals.cos().mean());
+        residualsMod = (twoPi / thetaStep) * residualsMod;
+        double circularMean = std::atan2(residualsMod.sin().mean(), residualsMod.cos().mean());
 
         if (circularMean < 0) {
             circularMean += twoPi;
@@ -30,9 +28,9 @@ namespace accurate_ri {
     }
 
     Stats::LRResult refineFit(
-        const Eigen::ArrayXd &x, const Eigen::ArrayXd &y, const Eigen::ArrayXi& linesIdx, const double period
+        const Eigen::ArrayXd &x, const Eigen::ArrayXd &y, const NewMultiLineResult &multiLineResult, const double period
     ) {
-        const Eigen::ArrayXd shiftedY = y - linesIdx.cast<double>() * period;
+        const Eigen::ArrayXd shiftedY = y - multiLineResult.linesIdx.cast<double>() * period;
         const Stats::LRResult fitResult = Stats::linearRegression(x, shiftedY, true);
 
         return fitResult;
@@ -40,21 +38,22 @@ namespace accurate_ri {
 
     void computePeriodicResiduals(
         const Eigen::ArrayXd& x, const Eigen::ArrayXd& y, const double period, const double slope,
-        const double intercept
+        const double intercept, NewMultiLineResult& outResult
     ) {
-        buffer.residuals = y - (slope * x + intercept);
-        buffer.linesIdx = (buffer.residuals / period).round().cast<int>();
-        buffer.residuals = buffer.residuals - buffer.linesIdx.cast<double>() * period;
+        outResult.residuals = y - (slope * x + intercept);
+        outResult.linesIdx = (outResult.residuals / period).round().cast<int>();
+        outResult.residuals = outResult.residuals - outResult.linesIdx.cast<double>() * period;
     }
 
     Stats::LRResult PeriodicFit::fit(
         const Eigen::ArrayXd &x, const Eigen::ArrayXd &y, const double period, const double slopeGuess
     ) {
-        computePeriodicResiduals(x, y, period, slopeGuess, 0);
+        NewMultiLineResult multiLineResult;
+        computePeriodicResiduals(x, y, period, slopeGuess, 0, multiLineResult);
 
-        const double intercept = computeCircularMeanIntercept(buffer.residuals, period);
-        computePeriodicResiduals(x, y, period, slopeGuess, intercept);
+        const double intercept = computeCircularMeanIntercept(multiLineResult.residuals, period);
+        computePeriodicResiduals(x, y, period, slopeGuess, intercept, multiLineResult);
 
-        return refineFit(x, y, buffer.linesIdx, period);
+        return refineFit(x, y, multiLineResult, period);
     }
 }
