3d4d9e428f714bb9b6ae9c00fc643f5a0ab47105
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-08-12 14:17:37 +0200

    Added migration for experiment ablation flags

---
+++ examples/resources/migration_5.sql
@@ -0,0 +1,5 @@
+ALTER TABLE experiment 
+    ADD COLUMN use_hough_continuity boolean NOT NULL DEFAULT true,
+    ADD COLUMN use_scanline_conflict_solver boolean NOT NULL DEFAULT true,
+    ADD COLUMN use_vertical_heuristics boolean NOT NULL DEFAULT true,
+    ADD COLUMN use_horizontal_heuristics boolean NOT NULL DEFAULT true;
