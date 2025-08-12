159817d5f27c79c46dfec1eeba3a975ef2e91f6d
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-08-12 14:19:22 +0200

    Modified migration file

--- examples/resources/migration_5.sql
+++ examples/resources/migration_5.sql
@@ -1,5 +1,11 @@
 ALTER TABLE experiment 
-    ADD COLUMN use_hough_continuity boolean NOT NULL DEFAULT true,
-    ADD COLUMN use_scanline_conflict_solver boolean NOT NULL DEFAULT true,
-    ADD COLUMN use_vertical_heuristics boolean NOT NULL DEFAULT true,
+    ADD COLUMN use_hough_continuity boolean NOT NULL DEFAULT true;
+
+ALTER TABLE experiment 
+    ADD COLUMN use_scanline_conflict_solver boolean NOT NULL DEFAULT true;
+
+ALTER TABLE experiment 
+    ADD COLUMN use_vertical_heuristics boolean NOT NULL DEFAULT true;
+
+ALTER TABLE experiment 
     ADD COLUMN use_horizontal_heuristics boolean NOT NULL DEFAULT true;
