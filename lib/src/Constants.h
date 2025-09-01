49f985bc8d3fb6ad693decff134ed16cc923ef28
Samuel Soutullo <samuelsoutullosobral@gmail.com>
2025-09-01 18:39:18 +0200

    Clean up unneeded constants

--- lib/src/Constants.h
+++ lib/src/Constants.h
@@ -9,12 +9,6 @@ namespace accurate_ri::Constant {
     constexpr uint64_t VERTICAL_MAX_FIT_ATTEMPTS = 10;
 
     constexpr int32_t MAX_RESOLUTION = 10000;
-
-    // TODO infer this more smartly, use gaussian thingies or something to derive bounds w.r.t to points
-    constexpr int32_t MAX_RESOLUTION_DELTA = 50;
-
-    // TODO derive this more elegantly, assuming a max offset or something
     constexpr double INV_RANGES_BREAK_THRESHOLD = 1e-2;
-
     constexpr int32_t HORIZONTAL_MIN_POINTS_PER_SCANLINE = 16;
 }
